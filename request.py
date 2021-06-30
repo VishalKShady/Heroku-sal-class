5cd829af024a6c0d41e036464cb0819e3f148838
VishalKShady <71310463+VishalKShady@users.noreply.github.com>
2021-06-30 13:31:46 +0530

    Update request.py

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://localhost:8000/predict_api'
+url = 'http://localhost:5000/predict_api'
 r = requests.post(url,json={'experience':5, 'test_score':8, 'interview_score':7})
 
 print(r.json())
